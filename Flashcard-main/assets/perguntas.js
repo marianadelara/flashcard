16cf5d4df7b91f8f799a79c22c21144518dcfd73
marianadelara <mariana.lara.neves@escola.pr.gov.br>
2025-06-27 12:18:09 -0300

    Add files via upload

---
+++ Flashcard-main/assets/perguntas.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    criaCartao(
+        'Gorillaz', 
+        'Quem são os criadores da banda virtual Gorillaz?', 
+        'A banda foi criada por Damon Albarn (músico) e Jamie Hewlett (cartunista) em 1998.'
+    );
+
+    criaCartao(
+        'Vocalista', 
+        'Qual é o nome verdadeiro do vocalista 2D?', 
+        'O nome verdadeiro de 2D é Stuart Pot.'
+    );
+
+    criaCartao(
+        'Músicas', 
+        'Qual a música mais famosa da banda?', 
+        'A música que se popularizou bastante, e até os dias de hoje é a mais famosa da banda, é Feel Good Inc., do álbum Demon Days, lançado em 2005.'
+    );
+
+    criaCartao(
+        'Álbum', 
+        'Qual o álbum mais famoso da banda?', 
+        'O álbum mais famoso da banda é o álbum Demon Days, lançado em 2005.'
+    );
+});
